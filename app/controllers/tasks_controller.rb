84b84962438572efd9bee5366cd9e95f7d902102
Noah <yasskin@icloud.com>
2016-09-05 16:47:33 -0700

    attempted to add CRUD functionality to tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      redirect_to list_path(@task.list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(task_params)
+    if @task.update(task_params)
+      redirect_to list_path(@task.list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task.destroy
+    redirect_to lists_path
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:description)
+  end
+
+end
