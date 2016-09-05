4ad1e0c9acdf1a8bf2961609a035c3b56cd88ce5
Noah <yasskin@icloud.com>
2016-09-05 15:28:22 -0700

    add new action to lists controller and new view with form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,24 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     render :show
   end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
